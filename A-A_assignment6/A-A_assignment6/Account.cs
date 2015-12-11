1296e99ad9624ddb0670ce4490ce2c1d6f712e8c
Mike Hovfing Heerwagen <cph-mh39@cphbusiness.dk>
2015-12-11 16:13:23 +0100

    DOneski

---
+++ A-A_assignment6/A-A_assignment6/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace A_A_assignment6
+{
+    class Account
+    {
+        public double balance = 0;
+        public double amount = 0;
+
+        public String deposit(double amount)
+        {
+            Contract.Requires(amount > 0);
+            Contract.Ensures(Contract.OldValue(balance) + amount == balance);
+            balance += amount;
+            return "Balance: " + balance;
+        }
+
+
+        public String withdraw(double amount)
+        {
+
+            Contract.Requires(amount > 0);
+            Contract.Requires(balance >= amount);
+            Contract.Ensures(Contract.OldValue(balance).Equals(amount + balance));
+            this.amount = amount;
+            balance = balance - amount;
+
+            return "Balance: " + balance;
+        }
+        public double getAmount { get { return amount; } }
+        public double getBalance { get { return balance; } }
+        //  [ContractInvariantMethod]
+        //  private void invariant() {
+
+        //    Contract.Invariant(getAmount <= getBalance);  
+
+        // }
+    
+}
+}
---
+++ A-A_assignment6/A-A_assignment6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_A_assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+                Account account = new Account();
+                Console.Write("200 Deposited: " + account.deposit(200));
+                Console.Write("100 withdrawed: " + account.withdraw(300));
+                Console.ReadLine();
+      
+    }
+}
